740f27f03ffc9faed3866bc539bcf42f77b6e24e
zronghui <825503975@qq.com>
2020-07-04 14:30:25 +0800

    feat(migu): 添加咪咕提醒，每 15 分钟一次

---
+++ migu.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pretty_errors
+from cqhttp import CQHttp
+from icecream import ic
+import schedule
+
+pretty_errors.activate()
+
+bot = CQHttp(api_root='http://127.0.0.1:5700/',
+             # access_token='your-token',
+             # secret='your-secret'
+             )
+
+
+def miguNotify():
+    message = '咪咕打卡'
+    bot.send(event={}, message=message, user_id='825503975')
+
+
+schedule.every(15).minutes.do(miguNotify)
